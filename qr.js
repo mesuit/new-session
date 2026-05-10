7c6350eeb5e729c203a7ac70d82732ba56ca6814
mesuit <makatech92@gmail.com>
2026-05-10 11:18:32 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -11,7 +11,7 @@ const {
     delay,
     fetchLatestBaileysVersion,
     makeCacheableSignalKeyStore,
-} = require("toxic-baileys");
+} = require("whiskeysockets/baileys");
 
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
